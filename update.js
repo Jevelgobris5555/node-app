8661151abb627e0cc27ec108502823694c69e08a
Jevelgobris5555 <115777013+Jevelgobris5555@users.noreply.github.com>
2022-10-20 13:11:32 +0800

    Create update.js

---
+++ update.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const update = function(note, OldNote){
+    const oNote = JSON.parse(oldNote);
+
+    const newNote = oNote.map(function(n, index){
+        if(n.id == note.id){
+            n.title = note.title
+            n.body = note.body
+        }
+        return n
+    })
+    fs.writeFileSync('./note.txt', JSON.stringify(newNote));
+}
+module.exports = update;
+
+
